1e1cae8a11e50fb0def735b3ba81bb178eff1409
saikumar <venkatsai.terminator@gmail.com>
2026-04-23 19:03:49 +0530

    Added childelement and setting changes

---
+++ tests/Childelements.spec.ts
@@ -0,0 +1,19 @@
+import {test} from '@playwright/test'
+
+test.beforeEach(async({page})=>{
+    await page.goto("http://localhost:4200/");
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layout').click();
+
+})
+
+test ('locating child elemnts', async({page})=>{
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();
+
+    await page.locator('nb-card').getByRole('button',{name:"Sign In"}).first().click();
+
+    await page.locator('nb-card').nth(3).getByRole('button', {name:"Sign In"}).click();
+
+
+})
